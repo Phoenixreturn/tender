6aa3746bd055518daca7465fbd55146ef1459f16
Phoenixreturn <rob.gallyamov@gmail.com>
2018-04-14 17:01:48 +0500

    sql connection

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDebug>
+#include <QtSql>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    qDebug() << MAXBUFFERSIZE;
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL", "mydb");
+    db.setHostName("176.57.216.145");
+    db.setDatabaseName("cl132598_tender");
+    db.setUserName("cl132598_tender");
+    db.setPassword("t8b54g2rg");
+    bool ok = db.open();
+
+    qDebug() << ok;
 
     return a.exec();
 }

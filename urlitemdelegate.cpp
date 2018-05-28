45a7f589db2769a953b2476d7c81a4758675ceea
Phoenixreturn <rob.gallyamov@gmail.com>
2018-05-28 08:14:11 +0500

    UrlDelegate

--- urlitemdelegate.cpp
+++ urlitemdelegate.cpp
@@ -15,20 +15,19 @@ UrlItemDelegate::UrlItemDelegate(QWidget *parent)
 {
     url = "";
     tmr = NULL;
-    editState = false;
-    connect(this, SIGNAL(changeEditState(bool)), this, SLOT(setEditState(bool)));
 }
 
 QWidget *UrlItemDelegate::createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
     QLineEdit *editor = new QLineEdit(parent);
-    editor->setValidator(new UrlValidator);
     return editor;
 }
 
 void UrlItemDelegate::setEditorData(QWidget *editor, const QModelIndex &index) const
 {
-    QStyledItemDelegate::setEditorData(editor, index);
+    QString value = index.model()->data(index, Qt::DisplayRole).toString();
+    QLineEdit* line = static_cast<QLineEdit*>(editor);
+    line->setText(value);
 }
 
 void UrlItemDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
@@ -56,7 +55,14 @@ void UrlItemDelegate::setModelData(QWidget *editor, QAbstractItemModel *model, c
 {
     QLineEdit* lineEdit = qobject_cast<QLineEdit*>(editor);
     QString temp = lineEdit->text();
-    QStyledItemDelegate::setModelData(editor, model, index);
+    int length = temp.size();
+    UrlValidator* validator = new UrlValidator();
+    if(validator->validate(temp, length) == QValidator::Acceptable) {
+        model->setData(index, temp);
+    } else {
+        lineEdit->setText("");
+        model->setData(index, "");
+    }
 }
 
 bool UrlItemDelegate::editorEvent(QEvent *event, QAbstractItemModel *model,
@@ -68,37 +74,23 @@ bool UrlItemDelegate::editorEvent(QEvent *event, QAbstractItemModel *model,
             tmr->stop();
             delete tmr;
         }
-
         return QStyledItemDelegate::editorEvent(event, model, option, index);
     }
 
     if(event->type() == QEvent::MouseButtonPress) {
-//        if(!this->getEditState()) {
             tmr = new QTimer();
             tmr->setInterval(200);
             url = index.data(Qt::DisplayRole).toString();
             connect(tmr, SIGNAL(timeout()), this, SLOT(openUrl()));
             tmr->start();
-
             return true;
-//        }
     }
 
     return QStyledItemDelegate::editorEvent(event, model, option, index);
-}
-
-bool UrlItemDelegate::getEditState()
-{
-    return editState;
 }
 
 void UrlItemDelegate::openUrl()
 {
     QDesktopServices::openUrl(url);
     tmr->stop();
-}
-
-void UrlItemDelegate::setEditState(bool state)
-{
-    editState = state;
 }
--- urlitemdelegate.h
+++ urlitemdelegate.h
@@ -9,26 +9,18 @@ class UrlItemDelegate : public QStyledItemDelegate
     Q_OBJECT
 public:
     UrlItemDelegate(QWidget *parent = 0);
-//    virtual void	destroyEditor(QWidget *editor, const QModelIndex &index) const;
-    virtual QWidget *	createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const;
+    virtual QWidget* createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const;
     virtual void setEditorData(QWidget * editor, const QModelIndex & index) const;
-//    virtual void setModelData(QWidget * editor, QAbstractItemModel * model, const QModelIndex & index) const;
     void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const;
-//    QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const;
     void setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const;
 protected:
     virtual bool editorEvent(QEvent *event, QAbstractItemModel *model,
                              const QStyleOptionViewItem &option, const QModelIndex &index);
 private:
-    bool getEditState();
     QTimer *tmr;
     QString url;
-    bool editState;
 private slots:
     void openUrl();
-    void setEditState(bool);
-signals:
-    void changeEditState(bool);
 };
 
 #endif // URLITEMDELEGATE_H
